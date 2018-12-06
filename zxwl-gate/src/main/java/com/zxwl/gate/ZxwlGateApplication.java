fd37d2598c71501434e406e0190187b868ab9887
dingweiwei <413061184@qq.com>
2018-12-06 16:07:13 +0800

    学习cloud

---
+++ zxwl-gate/src/main/java/com/zxwl/gate/ZxwlGateApplication.java
@@ -0,0 +1,12 @@
+package com.zxwl.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxwlGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxwlGateApplication.class, args);
+	}
+}
---
+++ zxwl-gate/src/test/java/com/zxwl/gate/ZxwlGateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxwl.gate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZxwlGateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
